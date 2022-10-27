d6308c7dbf9b8d242f0dab5d5bc67bd354c4b49d
Brooke Hamilton <brookehamilton@gmail.com>
2022-10-27 11:38:24 -0500

    initial package setup

---
+++ imaginator/__init__.py
@@ -0,0 +1,7 @@
+"""
+Imaginator
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Brooke Hamilton'
+__credits__ = 'Brooke Hamilton'
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='imaginator',
+    version='0.1.0',    
+    description='Bringing kids\' artwork to life using Stable Diffusion',
+    url='https://github.com/brookehamilton/imaginator',
+    author='Brooke Hamilton',
+    author_email='brookehamilton@gmail.com',
+    license='BSD 2-clause',
+    packages=['imaginator'],
+    install_requires=[
+                    'click',
+                    'diffusers',
+                    'huggingface-hub',
+                    'numpy',
+                    'Pillow',
+                    'scipy',
+                    'tokenizers',
+                    'torch',
+                    'transformers',                 
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
