0fecb0a477ee8d6853e602e8959cefdce52d261b
Brooke Hamilton <brookehamilton@gmail.com>
2022-11-08 13:59:02 -0600

    Added seed, strength, guidance_scale, and num_inference_steps to gradio interface

--- demo.py
+++ demo.py
@@ -12,24 +12,37 @@ import PIL
 
 pipe = create_pipeline()
 
-def run_gradio(starting_image: PIL.Image.Image, prompt: str):
+def run_gradio(starting_image: PIL.Image.Image,
+                prompt: str,
+                seed: int,
+                strength: float,
+                guidance_scale: float,
+                num_inference_steps: int):
+
     images = run_pipeline(pipe,
                 prompt=prompt,
                 init_image=starting_image,
-                strength=0.75,
-                guidance_scale=7.5,
+                strength=strength,
+                guidance_scale=guidance_scale,
                 negative_prompt=None,
-                num_inference_steps=15,
-                seed=102)
+                num_inference_steps=num_inference_steps,
+                seed=seed)
 
     return images[0]
 
 demo = gr.Interface(
     fn=run_gradio,
-    inputs=[gr.Image(type="pil"), "text"],
-    #inputs=["text", "text", "text",  "text", "text", "number", "number",
-            #gr.Slider(0.5, 1), gr.Slider(7, 8), "checkbox"],
+    inputs=[gr.Image(type="pil"),                                           # starting_image
+            "text",                                                         # prompt
+            gr.Number(value=455),                                           # seed
+            gr.inputs.Slider(0.0, 1.0, step=0.05, default=0.75,             # strength
+                label='Strength (how much to transform the starting image; higher values less faithful to starting image'),
+            gr.inputs.Slider(6, 9, step=0.25, default=7.5,                  # guidance scale
+                label='Guidance Scale (how much to match text prompt; higher values match text more)'),
+            gr.Number(value=15, label='Num Inference Steps', precision=0)   # num_inference_steps
+            ],
     outputs=["image"],
     )
 
-demo.launch(share=True)
+demo.launch()
+#demo.launch(share=True)
